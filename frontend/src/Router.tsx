53eafb2a7433607609ec18ebfbd4db3f39607887
sheker <hugoduval.gml@gmail.com>
2025-03-22 13:51:06 +0100

    temp 404 page and customed app in config files

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,11 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
 
 const AppContent = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
